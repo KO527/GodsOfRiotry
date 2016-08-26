db31e3f82049cb547b1c7d8f4e60ae02039f9a1d
KO527 <Kwajo.oseiakoto@gmail.com>
2016-08-26 00:20:32 -0400

    added users helper

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,26 @@
+module UsersHelper
+
+	def admin_user
+	    redirect_to(root_url) unless current_user.admin?
+	end
+
+	def correct_user 
+
+	end
+
+	def remember(user)
+   	    user.remember
+   	    cookies.permanent.signed[:user_id] = user.id
+   	    cookies.permanent[:remember_token] = user.remember_token
+	end
+
+	def log_in(user)
+	    session[:user_id] = user.id
+	end
+
+	def log_out
+	    session.delete(:user_id)
+	    @current_user = nil
+	end
+
+end
