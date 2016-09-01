8fb84e343a7bd69878a66ff7d2bac03e11e2bddc
KO527 <Kwajo.oseiakoto@gmail.com>
2016-09-01 16:26:19 -0400

    asserted presence of site_layout/views

---
+++ test/controllers/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "root_path leads to home page" do
+       get "/"
+       assert_response :success
+       assert_template partial: 'songs/_songs'       
+       assert_template partial: 'gor_clothing/_show'
+       assert_template partial: 'event_tickets/_event_tickets'
+  end
+ 
+end
+
