60176c566f77c63205ea4961784d67406d75154a
KO527 <Kwajo.oseiakoto@gmail.com>
2016-09-01 15:20:46 -0400

    updated gor_clothing model

--- app/models/gor_clothing.rb
+++ app/models/gor_clothing.rb
@@ -1,9 +1,13 @@
 class GorClothing < ActiveRecord::Base
 	has_attached_file :image, :styles => {large: "90x90>", medium: "70x70>", thumb: "45x45#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-	validates :quantity 
-	validates :sizes
-	validates :price
-	validates :description
-	validates :image
+	
+	VALID_PRICE_REGEX = /\$?\d*\.?\d+/
+
+	validates :quantity, presence: true
+	validates :sizes, presence: true
+	validates :price, format: {with: VALID_PRICE_REGEX} 
+	validates :description, presence: true
+	validates :colors_available, presence: true
+	validates :image, presence: true
 end
