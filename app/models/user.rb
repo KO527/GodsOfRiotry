ab05a3dc84c2485605e77024b651bbc6ded2d373
KO527 <Kwajo.oseiakoto@gmail.com>
2016-10-08 16:27:34 -0400

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,8 @@ class User < ActiveRecord::Base
 		BCrypt::Password.create(string, cost: cost)
 	end
 
+	# def User.digest(soundclou)
+
 	def User.new_token
 		SecureRandom.urlsafe_base64
 	end
@@ -40,11 +42,32 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, nil)
 	end
 
-	def self.create_from_soundcloud(access_token)
 
-		create! do |user|
-			user.soundcloud_user_id = soundcloud_user["id"]
-			user.soundcloud_access_token = access_token["access_token"]
-		end
+	#put inside of soundcloud service
+	def self.sign_in_from_omniauth(auth)
+		find_by(provider: auth['provider'], uid: auth['uid']) || create_from_omniauth_hash(auth)
 	end
+	
+	def self.create_from_omniauth(auth)
+		#create new user if self.soundcloud_user_playlist !== 
+		# return "hello" unless auth_hash
+		create!(
+			provider: auth['provider'],
+			uid: auth['uid'],
+			provider_location: auth['provider_location'],
+			provider_full_name: auth['provider_full_name'],
+			provider_nickname: auth['provider_nickname'],
+			access_token: auth['credentials']['token']
+		)
+	end
+
+
+
+	# def self.create_from_soundcloud(access_token)
+
+	# 	create! do |user|
+	# 		user.soundcloud_user_id = soundcloud_user["id"]
+	# 		user.soundcloud_access_token = access_token["access_token"]
+	# 	end
+	# end
 end
