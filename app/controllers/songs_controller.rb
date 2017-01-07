7519ce9c068b6da86c1fbcad5998095f57938f83
KO527 <Kwajo.oseiakoto@gmail.com>
2017-01-07 00:02:58 -0500

    added songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,22 @@
 class SongsController < ApplicationController
+
+before_action :admin_user	only: [:show]
+
+def show
+	# @songs = User.find(params[:uid])  shows songs that user has a preference for...songs replayed etc, etc
+end
+
+def SetupPlaylist
+	#Figure out how to setup a playlist for user's shopping experience...etc randomized mixture of users preconcieved taste and creator's recommendation list
+	#
+end
+
+def favorite
+	#Allow a user to favorite a song and save the song
+end
+
+def shuffle
+	#Reshuffles songs so that new playlist comes into play
+end
+
 end
