0e60459f1e6e6a8cb878c4a51b321aa3874aa0e5
KO527 <Kwajo.oseiakoto@gmail.com>
2017-01-11 23:46:14 -0500

    updated song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,12 @@
 class Song < ActiveRecord::Base
+
+	belongs_to :artists
+	belongs_to :preferences
+	belongs_to :playlist
+	belongs_to :subscriber_id, class_name: "User"
+
+	validates :name, presence: true, length: {maximum: 50}, unique: {case_sensitive: false}
+	validates :genre, presence: true, inclusion: {in: %w(#list_of_genres), message: "%{value} is not a valid genre"}
+	validates :favorite_tally, presence: true, numericality: {only_integer: true}
+
 end
