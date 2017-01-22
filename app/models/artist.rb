6a5c7621b43c0c852ae6dc5dc0f9f1efb909d040
KO527 <Kwajo.oseiakoto@gmail.com>
2017-01-21 23:11:23 -0500

    updated artist model file

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,13 +1,8 @@
 class Artist < ActiveRecord::Base
 	has_many :songs
 	belongs_to :playlist
-
+	belongs_to :event_ticket
 	validates :name, presence: true, length: {maximum: 50}, unique: {case_sensitive: false}
-	validates :favorite_tally, presence: true #associated with artist's song's that have been favorited tallied
-
-	private
-
-		def Event_ticket_queried
-			#if event_ticket is queried then associate interest with artist
-		end
+	validates :fav_count, presence: true, numericality: {integer: true}, allow_nil: true
+	
 end
