a3698bba73b1cb2d347d20bf7658d43f2cd4ddf2
KO527 <Kwajo.oseiakoto@gmail.com>
2017-01-21 23:16:40 -0500

    updated song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
-
+	enum status: [:favorited, :unfavorited]
+	
 	belongs_to :artists
 	belongs_to :preferences
 	belongs_to :playlist
@@ -7,6 +8,6 @@ class Song < ActiveRecord::Base
 
 	validates :name, presence: true, length: {maximum: 50}, unique: {case_sensitive: false}
 	validates :genre, presence: true, inclusion: {in: %w(#list_of_genres), message: "%{value} is not a valid genre"}
-	validates :favorite_tally, presence: true, numericality: {only_integer: true}
+	validates :fav_count, presence: true, numericality: {only_integer: true}, allow_nil: true
 
 end
