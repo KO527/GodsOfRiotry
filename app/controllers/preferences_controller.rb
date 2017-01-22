0cf1394e805c56ed91954c322cdfd33aef71f153
KO527 <Kwajo.oseiakoto@gmail.com>
2017-01-21 23:38:02 -0500

    added preferences controller

---
+++ app/controllers/preferences_controller.rb
@@ -0,0 +1,7 @@
+class PreferencesController < ApplicationController
+
+	def show
+		# @songs = User.find(params[:uid])  shows songs that user has a preference for...songs replayed etc, etc
+	end
+
+end
