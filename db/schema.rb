752833a7af64381475d3a55a42a872a99634f704
KO527 <Kwajo.oseiakoto@gmail.com>
2017-01-27 14:07:18 -0500

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120134231) do
+ActiveRecord::Schema.define(version: 20170127184522) do
+
+  create_table "acts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "acts_event_ticket", id: false, force: :cascade do |t|
+    t.integer "event_ticket_id", null: false
+    t.integer "act_id",          null: false
+  end
+
+  add_index "acts_event_ticket", ["act_id", "event_ticket_id"], name: "index_acts_event_ticket_on_act_id_and_event_ticket_id"
+  add_index "acts_event_ticket", ["event_ticket_id", "act_id"], name: "index_acts_event_ticket_on_event_ticket_id_and_act_id"
 
   create_table "artists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "fav_count"
   end
+
+  create_table "artists_event_ticket", id: false, force: :cascade do |t|
+    t.integer "event_ticket_id", null: false
+    t.integer "artist_id",       null: false
+  end
+
+  add_index "artists_event_ticket", ["artist_id", "event_ticket_id"], name: "index_artists_event_ticket_on_artist_id_and_event_ticket_id"
+  add_index "artists_event_ticket", ["event_ticket_id", "artist_id"], name: "index_artists_event_ticket_on_event_ticket_id_and_artist_id"
+
+  create_table "event_ticket_teams", id: false, force: :cascade do |t|
+    t.integer "event_ticket_id", null: false
+    t.integer "team_id",         null: false
+  end
+
+  add_index "event_ticket_teams", ["event_ticket_id", "team_id"], name: "index_event_ticket_teams_on_event_ticket_id_and_team_id"
+  add_index "event_ticket_teams", ["team_id", "event_ticket_id"], name: "index_event_ticket_teams_on_team_id_and_event_ticket_id"
 
   create_table "event_tickets", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170120134231) do
     t.datetime "happening"
     t.string   "name"
     t.integer  "fav_count"
+    t.integer  "user_id"
   end
 
   add_index "event_tickets", ["happening"], name: "index_event_tickets_on_away_team_and_away_team_and_happening", unique: true
@@ -33,6 +63,7 @@ ActiveRecord::Schema.define(version: 20170120134231) do
   add_index "event_tickets", ["happening"], name: "index_event_tickets_on_home_team_and_away_team_and_happening", unique: true
   add_index "event_tickets", ["name", "happening"], name: "index_event_tickets_on_artist_name_and_name_and_happening", unique: true
   add_index "event_tickets", ["name", "happening"], name: "index_event_tickets_on_performer_name_and_name_and_happening", unique: true
+  add_index "event_tickets", ["user_id"], name: "index_event_tickets_on_user_id"
 
   create_table "gor_clothings", force: :cascade do |t|
     t.datetime "created_at",         null: false
@@ -47,29 +78,35 @@ ActiveRecord::Schema.define(version: 20170120134231) do
     t.integer  "quantity"
   end
 
-  create_table "performers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "performers", [nil], name: "index_performers_on_name"
-
   create_table "playlists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "playlists", ["user_id"], name: "index_Playlists_on_user_id"
 
   create_table "preferences", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "fav_count"
+    t.integer  "user_id"
   end
 
+  add_index "preferences", ["user_id"], name: "index_Preferences_on_user_id"
+
   create_table "songs", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "fav_count"
+    t.integer  "playlist_id"
+    t.integer  "preference_id"
+    t.integer  "artist_id"
   end
+
+  add_index "songs", ["artist_id"], name: "index_Songs_on_artist_id"
+  add_index "songs", ["playlist_id"], name: "index_Songs_on_playlist_id"
+  add_index "songs", ["preference_id"], name: "index_Songs_on_preference_id"
 
   create_table "teams", force: :cascade do |t|
     t.datetime "created_at", null: false
