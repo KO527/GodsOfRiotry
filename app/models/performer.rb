95c6d00b76010accec0c563a856b6c01fd15c5e8
KO527 <Kwajo.oseiakoto@gmail.com>
2017-01-27 14:06:11 -0500

    updated routes

--- app/models/performer.rb
+++
@@ -1,6 +0,0 @@
-class Performer < ActiveRecord::Base
-	before_save {self.name = name.downcase}
-
-	has_and_belongs_to_many :event_tickets
-	validates :name, presence: true, length: {maximum: 50}, unique: {case_sensitive: false}
-end
