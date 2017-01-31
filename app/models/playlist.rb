1202a23a7fc7c67c4e3d2dbcc92c2170a08c10d7
KO527 <Kwajo.oseiakoto@gmail.com>
2017-01-31 10:45:27 -0500

    updated playlist model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,27 +1,36 @@
 class Playlist < ActiveRecord::Base
 	has_many :songs
-	has_many :favorited_songs, class_name: "Song"
-	belongs_to :subsriber_id, class_name: "User"
+	has_many :favorited_songs, class_name: "Song", where {status: favorited}
+	belongs_to :subscriber_id, class_name: "User", foreign_key: "user"
 
-	Playlist.joins(:songs).merge(Song.users_with_shared_song_interests.favorited_songs.distint_songs.song_select_range.take(2))
-
+	# Playlist.joins()
+	
 	private
 
+		# fav_artist_songs_select  = Songs.where(artists: user.favorited_artists).take(2)
+		# recommended_song_select = Songs.joins(:preferences).merge(Preference.with_shared_song_interests.distinct_songs).take(2)
+		# fav_events_artist_song_select = Songs.where(artists: {events: user.favorited_events}).take(2)
+		user = User.find_by(params[:user_id])
 
 		def SetupPlaylist
 			#Figure out how to setup a playlist for user's shopping experience...etc randomized mixture of users preconcieved taste and creator's recommendation list
-			
+			Playlist.clear
+			Playlist << Songs.where(artists: user.favorited_artists).take(2)
+			if user.favorited_events.favorited_artists.any?
+				Playlist << Songs.where(artists: {events: user.favorited_events}).take(2)
+			else
+				Playlist << Songs.where(artists: user.favorited_artists).take(2)
+			end
+			Playlist << Songs.joins(:preferences).merge(Preference.with_shared_song_interests.distinct_songs).take(2)
 		end
 
-		def shuffle_playlist
+		# def shuffle_playlist
 		
-			recommend_song_select = users_with_shared_song_interests.favorited_songs.distinct_songs.song_select_range.take(2)
+		# 	self.class.unscope
+		# 	Playlist.reload
 
-			self.class.unscope
-			Playlist.reload
-
-			Playlist << self.class.fav_artist_song_select			
-			#Reshuffles songs so that new playlist comes into play
-		end	
+		# 	Playlist << self.class.fav_artist_song_select			
+		# 	#Reshuffles songs so that new playlist comes into play
+		# end	
 
 end
