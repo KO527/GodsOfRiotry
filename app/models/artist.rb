2b23fd295f4146a34877a3842992d4362b2df0d0
KO527 <Kwajo.oseiakoto@gmail.com>
2017-01-31 10:45:07 -0500

    updated artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,5 +5,11 @@ class Artist < ActiveRecord::Base
 	has_and_belongs_to_many :event_ticket
 	validates :name, presence: true, length: {maximum: 50}, unique: {case_sensitive: false}
 	validates :fav_count, presence: true, numericality: {integer: true}, allow_nil: true
-	
+
+
+	private
+
+
+		# user = User.find_by(params[:user_id])
 end
+ 
