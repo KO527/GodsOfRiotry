10156e0497457d4e1522b787894e6cb05d7eb6fa
KO527 <Kwajo.oseiakoto@gmail.com>
2017-01-31 10:46:06 -0500

    updated song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,24 +1,29 @@
 class Song < ActiveRecord::Base
 	enum status: [:favorited, :unfavorited]
 	
-	belongs_to :artists
-	belongs_to :preferences
-	belongs_to :playlist
-	belongs_to :subscriber_id, class_name: "User"
+	belongs_to :artists, foreign_key: "artist_id"
+	belongs_to :preferences, foreign_key: "preference_id"
+	belongs_to :playlist, foreign_key: "playlist_id"
 
 	validates :name, presence: true, length: {maximum: 50}, unique: {case_sensitive: false}
 	validates :genre, presence: true, inclusion: {in: %w(#list_of_genres), message: "%{value} is not a valid genre"}
 	validates :fav_count, presence: true, numericality: {only_integer: true}, allow_nil: true
 
-
-	scope :created_before, -> (time) {where("created_at < ?", time) if time.present?}
-	scope :users_with_shared_song_interests -> {where(preferences: :current_user.favorited_songs).distinct}
-	scope :song_select_range -> {where(fav_count: song_avg_range - 10 .. song_avg_range + 10)}
-	scope :distinct_songs -> {where.not(current_user.favorited_songs)}
+	scope :distinct_songs -> {where.not(subscriber_id.favorited_songs)}
 	
+	private
+	
+		def StatusCountUpdate
+			# preferences = song.preferences.all
+			song = Song.find_by(params[:id])
+			song.fav_count.increment! && song.favorited! if song.unfavorited? == true
+			song.reload
+		end
+
+
 	# associate models
 	#last 10 favorited songs
-		
-	song_avg_range = current_user.average("favorited_songs.fav_count") # last 10 favorited songs
+	# scope :song_select_range -> {where(fav_count: song_avg_range - 10 .. song_avg_range + 10)}		
+	# song_avg_range = subscriber_id.average("favorited_songs.fav_count") # last 10 favorited songs
 
 end
