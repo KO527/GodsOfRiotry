5cdd00f85cd0f6534ce03b93dc33a33613c49699
KO527 <Kwajo.oseiakoto@gmail.com>
2017-02-12 19:26:31 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
  Rails.application.routes.draw do
   
 
-  get 'event_tickets_api/create'
+  # get 'event_tickets_api/create'
 
-  get 'event_tickets_api/update'
+  # get 'event_tickets_api/update'
 
   root               'sessions#new'
   
@@ -17,28 +17,66 @@
   # get '/soundcloud/oauth-callback', to: 'sessions#omniauth_create'
 
 
-  resources :users do
-  	resources :preferences
+  # resources :users do
+  # 	resources :event_tickets, :songs, :gor_clothing, :preferences
+  # end
+
+  resources :preferences do
   	member do
-  		get :favorited_outfits
+  		get :favorited_outfits, :favorited_songs, :favorited_events
   	end
   end
 
-  get '/signup', to: 'users#new'
+  resources :gor_clothing do
+  	resources :images, only: [:show, :preview]
+  end
 
-  namespace :admin do
-  	resources :gor_clothings
-  end  	
+  resources :event_tickets do
+  	resources :performers, :artists
+  end
 
-constraints subdomain: 'api' do
-	namespace :api, path: '/' do
-		resources :event_tickets
-	end
-end
+  resources :artists do
+  	resources :songs
+  end
 
-# except gor_clothings#show
+
+ constraints subdomain: 'api' do
+ 	namespace :api, path: '/' do
+ 		resources :event_tickets
+ 	end
+ end
+
+ namespace :admin do
+  	resources :preferences do
+  		resources :event_tickets, :songs, :gor_clothing
+  	end
+  	
+  	resources :gor_clothing do
+  		resources :images, shallow: true
+  	end
   
-resources :users
- resources :songs
+	resources :event_tickets do
+		resources :performers, :artists
+	end
+
+ end
 
 end
+
+#   get '/signup', to: 'users#new'
+
+#   namespace :admin do
+#   	resources :gor_clothings
+#   end  	
+
+# constraints subdomain: 'api' do
+# 	namespace :api, path: '/' do
+# 		resources :event_tickets
+# 	end
+# end
+
+# # except gor_clothings#show
+  
+# resources :users
+#  resources :songs
+
