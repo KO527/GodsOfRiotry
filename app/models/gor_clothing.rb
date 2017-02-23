430b44b5c4111c8c5137fd66adadd1af8d23bd87
KO527 <Kwajo.oseiakoto@gmail.com>
2017-02-23 13:51:42 -0500

    updated gor_clothing

--- app/models/gor_clothing.rb
+++ app/models/gor_clothing.rb
@@ -1,14 +1,28 @@
 class GorClothing < ActiveRecord::Base
-	enum status: [:favorited, :unfavorited]
+	enum purchase_status: [:on_shelf, :purchased]
+	enum status: [:unfavorited, :favorited]
+	enum gender: [:male, :female, :androgynous]
 	has_attached_file :image, :styles => {large: "90x90>", medium: "70x70>", thumb: "45x45#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	
 	VALID_PRICE_REGEX = /\$?\d*\.?\d+/
 
-	validates :quantity, presence: true
-	validates :sizes, presence: true
-	validates :price, format: {with: VALID_PRICE_REGEX} 
+
+	validates :quantity, presence: true, numericality: {only_integer: true}
+	validates :sizes, presence: true, inclusion: {in: %w(S M L)}
+	validates :price, format: {with: VALID_PRICE_REGEX}
 	validates :description, presence: true
 	validates :colors_available, presence: true
-	validates :image, presence: true
+	
+	has_many :images
+
+	validates_associated :image
+	validates_presence_of :image
+	
+	private
+
+		def self.add_color(color)
+			
+		end
+
 end
