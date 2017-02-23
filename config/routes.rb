0d816b883ead17980904568dbe8790cd1466c0c5
KO527 <Kwajo.oseiakoto@gmail.com>
2017-02-23 15:09:20 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,76 +7,130 @@
 
   root               'sessions#new'
   
-  get '/gor_main', to: 'gor_main#main'
   get '/help', to: 'gor_main#help'
   get '/contact', to: 'gor_main#contact'
   get '/logout', to: 'sessions#destroy'
   get '/login', to: 'sessions#create'
-  get '/edit', to: 'users#edit'
   get '/sessions/:page' => "sessions#show"
+  get '/edit', to: 'users#edit'
+
   # get '/soundcloud/oauth-callback', to: 'sessions#omniauth_create'
 
 
-  # resources :users do
-  # 	resources :event_tickets, :songs, :gor_clothing, :preferences
-  # end
+   resources :users do
+   	resources :event_tickets
+   	resources :gor_clothing, except: [:new]
+   	resources :playlist, except: [:index, :show]
+   	member do
+   		resources :preferences, only: [:index, :update, :create, :destroy]
+   	end
+   end
 
-  resources :preferences do
-  	member do
-  		get :favorited_outfits, :favorited_songs, :favorited_events
-  	end
+   resource :playlist do
+   	member do
+	   	patch 'change_up' #Randomize what gets played while keeping kept_songs in the playlist
+	end
+
+	# def change_up
+	# @playlist = playlist.find_by(params[:id])
+	# @songs = @playlist.songs.all
+	# @songs.each do |song|
+   	#  	if song.kept_song!
+   	# 		songs_to_be_deleted << song
+   	# 	else
+   	# 	 	nextl
+   	# 	end
+   	# end
+          #     songs_to_be_deleted.destroy  => collapse songs in playlist through javascript
+   	# end
+   	#
+
+   	resources :songs do
+   		member do
+   			post 'keep_song' #mark as kept_song
+   			# def keep_song
+   				#update preferences
+   		end
+   	end
+   	# [:destroy]
+   end
+
+
+  resources :preferences, only: [:index, :update, :create, :destroy] do
+  		resources :gor_clothing, except: [:show, :new, :edit] do #create, destroy, index, update
+  			collection do
+  				patch 'update_wardrobe'
+  				get 'saved_wardrobes'
+  			end
+  		end
+  		resources :songs, except: [:show, :new, :edit]
+  		resources :event_tickets, except: [:edit]
   end
 
-  resources :gor_clothing do
-  	resources :images, only: [:show, :preview]
+  resources :gor_clothing, except: [:new] do
+  	member do
+  		resources :images, only: [:index, :show, :preview]
+  	end
   end
 
   resources :event_tickets do
-  	resources :performers, :artists
+  	collection do
+  		get 'search'
+  	end
+  	# resources :performers, except: [:new, :edit, update] -> will create, destroy, show under preferences
+  	# resources :artists, except: [:new, :edit, update]
   end
 
-  resources :artists do
-  	resources :songs
-  end
+  # resources :artists do
+  # 	resources :songs
+  # end
 
-
- constraints subdomain: 'api' do
- 	namespace :api, path: '/' do
- 		resources :event_tickets
- 	end
- end
 
  namespace :admin do
   	resources :preferences do
-  		resources :event_tickets, :songs, :gor_clothing
+  		member do
+  			resources :event_tickets, :songs, :gor_clothing
+  		end 
   	end
   	
-  	resources :gor_clothing do
-  		resources :images, shallow: true
+  	#Set admin restrictions
+  	# Linked app/views/gor_clothing/image_setup.html
+
+  	resources :gor_clothing, only: [:new, :create, :edit, :update, :destroy] do
+  		member do
+  			resources :images, only: [:new, :show, :edit, :update, :destroy, :index, :preview] #Images controller...how do we specify?
+  		end
   	end
-  
-	resources :event_tickets do
-		resources :performers, :artists
+
+
+  	#image upload --> new --> triggered by carrier_wave uploader--> preview action --> render admin_gor_clothing_image_path alert(is show picture ok?)
+  	#save image --> create --> 
+  	
+  	# def gor_clothing
+  	# 	params.require(gor_clothing_params).permit(:name, :description, :size, :colors_available, :gender, :picture)
+  	# end
+
+  	resources :users do
+  		member do
+			resources :playlists, only: [:create, :destroy, :show, :index]
+			resources :gor_clothing # Designed to specifically check what user enjoys
+  		end
 	end
 
+  
+	# resources :event_tickets do
+	# 	resources :performers, :artists
+	# end
+
  end
+
 
 end
 
 #   get '/signup', to: 'users#new'
 
-#   namespace :admin do
-#   	resources :gor_clothings
-#   end  	
 
-# constraints subdomain: 'api' do
-# 	namespace :api, path: '/' do
-# 		resources :event_tickets
-# 	end
-# end
 
 # # except gor_clothings#show
   
-# resources :users
-#  resources :songs
 
