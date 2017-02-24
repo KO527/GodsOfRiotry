a7cf34e476fa5d75d49e13487010eb42e4cc6062
KO527 <Kwajo.oseiakoto@gmail.com>
2017-02-24 10:33:33 -0500

    updated act.rb

--- app/models/act.rb
+++ app/models/act.rb
@@ -2,5 +2,5 @@ class Act < ActiveRecord::Base
 	before_save {self.name = name.downcase}
 
 	has_and_belongs_to_many :event_tickets
-	validates :name, presence: true, length: {maximum: 50}, unique: {case_sensitive: false}
+	validates :name, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false}
 end
