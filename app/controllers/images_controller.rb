ab16a1457b505c14f0cdd884e06be57140445968
KO527 <Kwajo.oseiakoto@gmail.com>
2017-02-28 19:17:21 -0500

    updated images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,11 +3,14 @@ class ImagesController < ApplicationController
 
 	validate :picture_size
 
+	def new
+		
+	end
 
 	private
 
 		
-		def Images_params
-			params.require(Images_params).permit(:type_of_image, :picture)
+		def Image_params
+			params.require(:Image).permit(:type_of_image, :picture)
 		end
 end
