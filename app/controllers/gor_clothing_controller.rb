087088a87a277adb2ffa0cfebf07a040bfb8006a
KO527 <Kwajo.oseiakoto@gmail.com>
2017-02-28 19:16:35 -0500

    updated gor_clothing controller

--- app/controllers/gor_clothing_controller.rb
+++ app/controllers/gor_clothing_controller.rb
@@ -1,6 +1,6 @@
 class GorClothingController < ApplicationController
 before_action :logged_in_user
-before_action :user_admin
+before_action :admin, only: [:update, :edit, :new, :create, :destroy]
 
 
 def index
@@ -13,35 +13,46 @@ end
 
 def new
      @Gor_Clothing = Gor_Clothing.new
+     Image.new
 end
 
-def create
-     @Gor_Clothing.new(Clothing_params)
-end
+ def create
+      @Gor_Clothing.new(Gor_Clothing_params)
+      if @Gor_Clothing.update(gor_clothing_params)
+      	render 'show'
+      else
+      	render 'new'
+      end
+ end
+#      	render preview_new_gor_clothing_path
+
 
 def edit
- 
+     @Gor_Clothing.find(params[:id]) #create edit page
 end
 
 def update
 	@Gor_Clothing = Gor_Clothing.find(params[:id])
-	if @Gor_Clothing.update_attributes(Clothing_params)
-		flash[:success] = "Profile updated"
-		redirect_to @Gor_Clothing
+	if @Gor_Clothing.update(Gor_Clothing_params)
+		flash[:success] = "Successful upload"
+		render 'show_detail'
 	else
 		render 'edit'
 	end
 end
 
 def destroy
-	
+	@Gor_Clothing = Gor_Clothing.find(params[:id])
+	@Gor_Clothing.destroy
+
+	redirect_to Gor_Clothing_path
 end
 
 
 private
 
-	def Clothing_params
-	    params.require(:Gor_Clothing).permit(:image, :price, :description, :quantity)
+	def Gor_Clothing_params
+	    params.require(:Gor_Clothing).permit(:images_attributes: [:picture, :type_of_image], :price, :description, :quantity, :gender, :size)
 	end
 
 end
