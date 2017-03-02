1e979a144b03b560df30fb31c583081f0a543503
KO527 <Kwajo.oseiakoto@gmail.com>
2017-03-02 14:55:25 -0500

    updated images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,10 +1,52 @@
 class ImagesController < ApplicationController
-
+	before_action :admin, only: [:preview, :new, :create, :update]
 
 	validate :picture_size
 
 	def new
+		@image = Image.new		
+	end
+
+	# def create
+	# 	@image = Image.new(Image_params)
 		
+	# 	render 'preview'
+	# end
+
+	def edit
+		@gor_clothing = Gor_clothing.find(params[:gor_clothing_id])
+		@gor_clothing.images.each do |image|
+			#append destroy_link to images through js
+		end
+	end
+
+	# Finished editing button updates Images
+	def update
+		@image = Images.find(params[:id])
+		@image.update_attributes(Image_params)
+		
+	end
+	
+	def index
+		@gor_clothing = Gor_Clothing.find(params[:gor_clothing_id])
+		@images = @gor_clothing.Images.all
+	end
+
+	def destroy
+	end
+
+	def preview #show form through js
+		@gor_clothing = Gor_clothing.find(params[:gor_clothing_id])
+
+		if @gor_clothing.new_record?
+			@image = Image.create({'type_of_image' => params[:type_of_image],
+				            'picture' => params[:picture]})
+		elsif @gor_clothing.persisted?
+			@image = Image.update({'type_of_image' => params[:type_of_image],
+						  'picture' => params[:picture]}) if @image.valid?
+		end
+
+		render 'detail_page'
 	end
 
 	private
