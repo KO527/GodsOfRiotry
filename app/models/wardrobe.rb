9d78ff1cc76a356c586486db83497a495dcdcf7c
KO527 <Kwajo.oseiakoto@gmail.com>
2017-03-02 14:58:01 -0500

    updated wardrobe model

--- app/models/wardrobe.rb
+++ app/models/wardrobe.rb
@@ -1,2 +1,9 @@
 class Wardrobe < ActiveRecord::Base
+
+	belongs_to :user
+	has_many :gor_clothings
+	validates_associated :gor_clothing
+	validates_presence_of :gor_clothing
+	accepts_nested_attributes_for :gor_clothing, allow_destroy: true, limit: 12, reject_if: proc {|attributes| attributes[:image][:type_of_image] != :show_picture || attributes[:image][:picture].blank?} on: :update
+
 end
