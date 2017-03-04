d3330538c3f2811999ba1bdac40bea622e445934
KO527 <Kwajo.oseiakoto@gmail.com>
2017-03-04 16:00:15 -0500

    updated images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -8,17 +8,21 @@ class ImagesController < ApplicationController
 		@image = Image.new		
 	end
 
-	# def create
-	# 	@image = Image.new(Image_params)
+	 # def create
+	 #  	@image = Image.new(Image_params)
 		
-	# 	render 'preview'
-	# end
-
+	 #  	render Gor_clothing/show
+	 #  end
+	def show
+		@image = Gor_clothing.find(params[:gor_clothing_id][:image][:show_picture])
+	end
+		# show.html.erb <% link_to image_tag image.picture, detail_page %>
+	
 	def edit
 		@gor_clothing = Gor_clothing.find(params[:gor_clothing_id])
 		@gor_clothing.images.each do |image|
 			#append destroy_link to images through js
-			image.
+			# image.
 		end
 	end
 
@@ -42,14 +46,14 @@ class ImagesController < ApplicationController
 
 		if @gor_clothing.new_record?
 			@image = Image.create({'type_of_image' => params[:type_of_image],
-				            'picture' => params[:picture]})
+			 	            'picture' => params[:picture]})
 		elsif @gor_clothing.persisted?
 			@image = Image.new({'type_of_image' => params[:type_of_image],
 						  'picture' => params[:picture]}) 
 			@image.save if @image.valid?
 		end
 
-		render 'detail_page'
+		render 'gor_clothing/show'
 	end
 
 	private
