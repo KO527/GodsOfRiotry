9cf2602e1a61d609b14e32c3a8f123f7d5e9f88d
KO527 <Kwajo.oseiakoto@gmail.com>
2017-03-04 01:07:41 -0500

    updated gor_clothing model

--- app/models/gor_clothing.rb
+++ app/models/gor_clothing.rb
@@ -9,19 +9,20 @@ class GorClothing < ActiveRecord::Base
 
 	belongs_to :user
 	belongs_to :wardrobe
-	
+
 	validates :quantity, presence: true, numericality: {only_integer: true}
 	validates :sizes, presence: true, inclusion: {in: %w(S M L)}
 	validates :price, format: {with: VALID_PRICE_REGEX}
 	validates :description, presence: true
 	validates :colors_available, presence: true
+	# validates :possible_matches, inclusion: {} allow_blank: true
 	
 	has_many :images
-
+	has_many :possible_matches, class_name: "Gor_clothing"
 	validates_associated :image
-	validates_presence_of :image, proc {|attributes| attributes[:type_of_image] == :show_picture if attributes['gor_clothing']['image'].blank?}
+	validates_presence_of :image, proc {|attributes| attributes[:type_of_image] == :show_picture if attributes['gor_clothing_id']['image'].blank?}
 	validates_uniqueness_of :image, scope: [:type_of_image]
-	accepts_nested_parameters_for :image, allow_destroy: true, reject_if: proc {|attributes| attributes[:type_of_image] !== :show_picture || :first_shot || :back_shot || :model_shot}
+	accepts_nested_parameters_for :image, allow_destroy: true, reject_if: proc {|attributes| attributes[:type_of_image] != :show_picture || :first_shot || :back_shot || :model_shot}
 
 	private
 		def self.add_color(color)
