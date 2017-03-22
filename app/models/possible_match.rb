63da7e7772fd589df976483e11c12821f5a314f0
KO527 <Kwajo.oseiakoto@gmail.com>
2017-03-22 05:04:24 -0400

    made minor tweaks to models and controllers

--- app/models/possible_match.rb
+++ app/models/possible_match.rb
@@ -1,6 +1,6 @@
 class PossibleMatch < ActiveRecord::Base
-	belongs_to :evaluated_piece, class_name: 'Gor_clothing'
 	belongs_to :suggested_piece, class_name: 'Gor_clothing'
-	validates :evaluated_piece_id, presence: true
+	belongs_to :contemplated_piece, class_name: 'Gor_clothing'
 	validates :suggested_piece_id, presence: true
+	validates :comtemplated_piece_id, presence: true
 end
