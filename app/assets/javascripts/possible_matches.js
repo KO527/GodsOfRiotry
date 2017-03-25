fe7204a6c327dd220c55167d7b1686bb744a63aa
KO527 <Kwajo.oseiakoto@gmail.com>
2017-03-25 12:30:21 -0400

    emptied possible_matches.js

--- app/assets/javascripts/possible_matches.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$("#edit_possible_matches").bind("ajax.success", function(event, data){
-		$(data).each(function(){
-			$('#possible_matches_' + this.id).addClass('destroy_link');
-		});
-	});
-});
