8c6f5080982ec62bb17662e3d0e1c58fc886e1e6
KO527 <Kwajo.oseiakoto@gmail.com>
2017-04-09 18:46:59 -0400

    updated admin.js to enable capability to remove images

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,15 +1,49 @@
 function append_destroy_links(id_clicked_on, targetElement){
 	$(id_clicked_on).bind("ajax.success", function(event, data){
 		$(data).each(function(){
-			("<%= escape_javascript(render('/destroy')) %>").insertBefore(targetElement + '_' + this.id);
+			if (targetElement == '#possible_matches')
+				$("<%= escape_javascript(render('possible_matches/destroy')) %>").insertBefore(targetElement + '_' + this.id);
+			else if (targetElement == '#gor_clothing_images')
+				$("<%= escape_javascript(render('images/destroy'))%>").insertBefore(targetElement + '_' + this.id)
 		});
 	});
 }
 $('document').ready(function(){
 	$('#edit_possible_matches').on('click', function(e){
-			append_destroy_links('#edit_possible_matches', '#possible_matches_');
+		append_destroy_links('#edit_possible_matches', '#possible_matches');
 	});
 	$('#edit_some_images').on('click', function(e){
-			append_destroy_links('#edit_some_images', '#gor_clothing_images_');
+		append_destroy_links('#edit_some_images', '#gor_clothing_images');
+		$("<%= escape_javascript(render('images/_images_finish_edit')) %>").insertAfter('#edit_some_images');	
 	});
+	$('i.icon-destroy-link').on('click', function(e){
+		$(this).addClass('set-for-deletion');
+	});
+
+	$('#Finish_Edit').on('click', deleteSetForDeletion, function(){
+		$("<%= escape_javascript(render('images/_images_finish_edit')) %>").detach();
+	}
+	// $('#new_piece').prompt some new file pop_up window
 });
+
+var deleteSetForDeletion = function(){
+	$.ajax({
+		type: 'DELETE',
+		url: '/admin/gor_clothing/:id/images/:id',
+		data: {
+		          destroy_link_icons: document.getElementsByClassName('icon-destroy-link');
+			collateral_images: $('collateral_images').has('i.set-for-deletion'); //All Elements with ClassName collateral-images and set-for-deletion
+		},
+		success: function(data){
+			$(data).each(function(){
+				$(this).Squish();
+				$(this).remove();
+			});
+		},	//remove 
+
+		error: function(data){
+			
+
+		}
+	})	
+}
