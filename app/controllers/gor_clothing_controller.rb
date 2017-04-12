b6f0dc35697cf68bc7ed351b7038ce5d7121a6bc
KO527 <Kwajo.oseiakoto@gmail.com>
2017-04-11 21:42:29 -0400

    updated gor_clothing_controller

--- app/controllers/gor_clothing_controller.rb
+++ app/controllers/gor_clothing_controller.rb
@@ -4,60 +4,72 @@ class GorClothingController < ApplicationController
 
 
 	def index
-	     @Gor_Clothings = Gor_Clothing.all
+	     @gor_clothings = Gor_Clothing.all
 	end
 
 	def show #js Show details Price and Description
-	     @Gor_Clothing = Gor_Clothing.find(params[:id])
+	     @gor_clothing = Gor_Clothing.find(params[:id])
 	end
 
 	def new
-	     @Gor_Clothing = Gor_Clothing.new
+	     @gor_clothing = Gor_Clothing.new
+	     if @gor_clothing.attribute_present?(:description, :size, :gender, :price, :quantity, :picture)
+	     	@gor_clothing.build(gor_clothing_params)
+	     	then render 'images/preview'
+	     end
 	end
 
-	 def create
-	      if @Gor_Clothing.new('description' => params[:description],
-	      			      'size' => params[:size],
-	      			      'gender' => params[:gender],
-	      	        	                'price' => params[:price],
-	      		                'quantitiy' => params[:quantity],
-	      		                'picture' => params[:image][:picture])
-	      	redirect_to 'images/new'
-	      else
-	      	render 'new'
-	      end
-	 end
+	def create
+	 	@gor_clothing = Gor_Clothing.new(gor_clothing_params)
+	          if @gor_clothing.new_record?
+		 	@image = @gor_clothing.image.create({'type_of_image' => params[:type_of_image],
+		 				     'picture' => params[:picture]})
+		 	@gor_clothing.save
+		 	render detail_admin_gor_clothing_path(@gor_clothing)
+		elsif @gor_clothing.persisted?
+		 	@image = Image.new({'type_of_image' => params[:type_of_image],
+		 				  'picture' => params[:picture]})
+		 	@image.save
+		 	render detail_admin_gor_clothing_path(@gor_clothing)
+		else
+		      	if @gor_clothing.attribute_present?(:description, :size, :gender, :price, :quantity, :picture)
+		      	render 'images/preview'
+		      	#show errors
+		end
+	end
 	#      	render preview_new_gor_clothing_path
 
 
 	def edit
-	     @Gor_Clothing = Gor_Clothing.find(params[:id])	      #render edit page
+	     @gor_clothing = Gor_Clothing.find(params[:id])	      #render edit page
 	end
 
 	def update
-		@Gor_Clothing = Gor_Clothing.find(params[:id])
-		if @Gor_Clothing.update(Gor_Clothing_params)
+		@gor_clothing = Gor_Clothing.find(params[:id])
+		if @gor_clothing.update(gor_clothing_params)
 			flash[:success] = "Successful upload"
-			render 'show_detail'
+			render detail_admin_gor_clothing_path(@gor_clothing)
 		else
 			render 'edit'
 		end
 	end
 
 	def destroy
-		@Gor_Clothing = Gor_Clothing.find(params[:id])
-		@Gor_Clothing.destroy
+		@gor_clothing = Gor_Clothing.find(params[:id])
+		@gor_clothing.destroy
 
-		redirect_to Gor_Clothing_path
+		redirect_to admin_gor_clothing_index_path
 	end
 	
-	# def detail
-	# end
-private
-
-	def Gor_Clothing_params
-	    params.require(:Gor_Clothing).permit(:price, :description, :quantity, :gender, :size, images_attributes: [:picture, :type_of_image, :_destroy])
+	def detail
+		@gor_clothing = Gor_Clothing.find(params[:id])
 	end
+
+	private
+
+		def gor_clothing_params
+		    params.require(:Gor_Clothing).permit(:price, :description, :quantity, :gender, :size, images_attributes: [:picture, :type_of_image, :_destroy])
+		end
 
 end
 
