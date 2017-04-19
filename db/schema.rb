f28f53adff96f7b921cbd0ab10f8a8cb2a44669e
KO527 <Kwajo.oseiakoto@gmail.com>
2017-04-18 23:59:56 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320110555) do
+ActiveRecord::Schema.define(version: 20170418052510) do
 
   create_table "acts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -138,8 +138,8 @@ ActiveRecord::Schema.define(version: 20170320110555) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "remember_digest"
     t.string   "password_digest"
     t.string   "email"
@@ -154,6 +154,7 @@ ActiveRecord::Schema.define(version: 20170320110555) do
     t.string   "access_token"
     t.boolean  "admin"
     t.string   "full_name"
+    t.integer  "gender",                   default: 0, null: false
   end
 
   create_table "wardrobes", force: :cascade do |t|
