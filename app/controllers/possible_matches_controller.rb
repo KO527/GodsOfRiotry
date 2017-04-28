ef5f79ee70bef53e8766139a5b96202080ac26c5
KO527 <Kwajo.oseiakoto@gmail.com>
2017-04-27 23:49:44 -0400

    updated possible_matches_controller

--- app/controllers/possible_matches_controller.rb
+++ app/controllers/possible_matches_controller.rb
@@ -1,5 +1,6 @@
 class PossibleMatchesController < ApplicationController
 	before_action :contemplated_piece
+	respond_to :html, :js
 
 	def new
 		@possible_matches = Gor_clothing.all
@@ -12,17 +13,18 @@ class PossibleMatchesController < ApplicationController
 	
 	def edit
 		@possible_matches = @gor_clothing.suggested_pieces(:all)
+		respond_with(@possible_matches)
 	end
 
 	def update
 	end
 
 	def destroy
-		@possible_matches = @gor_clothing.suggested_pieces() #all of the images with setForDeletion links
-		respond_to do |format|
-			format.js{}
-			format.html{}
-		end
+		# @possible_matches = @gor_clothing.suggested_pieces(:all) #all of the images with setForDeletion links
+		@gor_clothing = Gor_Clothing.find(params[:gor_clothing_id])
+		@possible_matches_selected = PossibleMatch.where(:suggested_pieces => params[:collateral_images]) #identify which images have setForDeletion
+		flash[:notice] = "Selected possible matches destroyed" if @possible_matches_selected.destroy
+		respond_with(@gor_clothing, :location => detail_gor_clothing_url)
 	end
 
 	private
