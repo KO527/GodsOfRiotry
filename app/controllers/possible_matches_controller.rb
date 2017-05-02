5a01a95438f5aa01f62e2321133490738c9b0f3d
KO527 <Kwajo.oseiakoto@gmail.com>
2017-05-02 00:02:48 -0400

    updated possible_matches controller

--- app/controllers/possible_matches_controller.rb
+++ app/controllers/possible_matches_controller.rb
@@ -1,18 +1,16 @@
 class PossibleMatchesController < ApplicationController
 	before_action :contemplated_piece
-	respond_to :html, :js
+	before_action :contemplated_pieces
+	respond_to :js, :html
 
 	def new
 		@possible_matches = Gor_clothing.all
-		respond_with()
 	end
 	
 	def index 
-		@possible_matches = @gor_clothing.suggested_pieces(:all)
 	end
 	
 	def edit
-		@possible_matches = @gor_clothing.suggested_pieces(:all)
 		respond_with(@possible_matches)
 	end
 
@@ -20,16 +18,21 @@ class PossibleMatchesController < ApplicationController
 	end
 
 	def destroy
-		# @possible_matches = @gor_clothing.suggested_pieces(:all) #all of the images with setForDeletion links
-		@gor_clothing = Gor_Clothing.find(params[:gor_clothing_id])
+		@possible_matches = @gor_clothing.suggested_pieces(:all) #all of the images with setForDeletion links
 		@possible_matches_selected = PossibleMatch.where(:suggested_pieces => params[:collateral_images]) #identify which images have setForDeletion
-		flash[:notice] = "Selected possible matches destroyed" if @possible_matches_selected.destroy
-		respond_with(@gor_clothing, :location => detail_gor_clothing_url)
+		if @possible_matches_selected.destroy
+			flash[:notice] = "Selected possible matches destroyed"
+			respond_with(@gor_clothing, @possible_matches, :action => possible_matches_edit_admin_gor_clothing(@gor_clothing, @possible_matches_selected))
+		end
 	end
 
 	private
 
-	def contemplated_pieces
-		@contemplated_pieces = PossibleMatch.find(params[:contemplated_piece_id]).all
-	end
+		def contemplated_piece
+			@gor_clothing = Gor_Clothing.find(params[:contemplated_piece_id])
+		end
+
+		def contemplated_pieces
+			@contemplated_pieces = PossibleMatch.find(params[:contemplated_piece_id]).all
+		end
 end
