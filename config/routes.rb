3d0928661a17283d14ad0f9c5ee5b8c46ca15244
KO527 <Kwajo.oseiakoto@gmail.com>
2017-05-05 00:00:12 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -107,6 +107,7 @@
   			get 'possible_matches/edit' => 'possible_matches#edit' #How does two different actions with the same URI differ so that the response knows one action should be the default action?
   			resources :possible_matches, except: [:show, :edit] do
   				match :destroy, to: 'possible_matches#destroy', via: [:delete], on: :collection
+  				
   			end
   			resources :images, except: [:edit, :update] do
   				match :edit_some, to: 'images#edit_some', via: [:get], on: :collection #Figure out relationship between update and destroy for edit_some
