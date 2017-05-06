ef090fddf7cbcf2b966575b68937eabc11539234
KO527 <Kwajo.oseiakoto@gmail.com>
2017-05-06 19:55:17 -0400

    updated helpers

--- app/helpers/gor_clothing_helper.rb
+++ app/helpers/gor_clothing_helper.rb
@@ -4,4 +4,8 @@ module GorClothingHelper
 		render 'images/preview' unless @gor_clothing.errors.count > 0
 	end
 
+	def find_clothing
+		@gor_clothing = Gor_Clothing.find(params[:gor_clothing_id])
+	end
+
 end
--- app/helpers/possible_matches_helper.rb
+++ app/helpers/possible_matches_helper.rb
@@ -1,2 +1,6 @@
 module PossibleMatchesHelper
+
+	def contemplated_piece
+		@gor_clothing = Gor_Clothing.find(params[:contemplated_piece_id])
+	end	
 end
