d23b2ce38005c3d946fe653474ba986c94b03ca2
KO527 <Kwajo.oseiakoto@gmail.com>
2017-05-09 00:00:16 -0400

    updated possible_matches_controller

--- app/controllers/possible_matches_controller.rb
+++ app/controllers/possible_matches_controller.rb
@@ -1,25 +1,27 @@
 class PossibleMatchesController < ApplicationController
-	before_action :contemplated_piece, only: [:index, :edit, :destroy]
+	before_action :contemplated_piece, only: [:new, :index, :create, :edit, :destroy]
 	before_action :admin, only: [:new, :edit, :destroy]
 	# before_action :contemplated_pieces
 	respond_to :js, :html
 
 	def new
-		# @gor_clothings = Gor_Clothing.all
-		# if @gor_clothing.
-		# 	@possible_matches = Gor_clothing.all
-		# else
+		@gor_clothings = Gor_Clothing.all
+		@possible_matches = PossibleMatch.new
+		respond_with(@gor_clothings)
 	end
 
 	def create
-		
+		@possible_matches = PossibleMatch.new(possible_matches_params)
+		flash[:notice] = "Possible matches for this gor_clothing piece created" if @possible_matches.save
+		redirect_to admin_gor_clothing_index_path
 	end
 
-	def index		
+	def index
 	end
 	
 	def edit
 		@possible_matches = @gor_clothing.suggested_pieces(:all)
+		respond_with(@gor_clothing, @possible_matches, location: admin_gor_clothing_index_path)
 	end
 
 	def update
@@ -33,5 +35,11 @@ class PossibleMatchesController < ApplicationController
 		end
  		respond_with(@gor_clothing, location: admin_gor_clothing_index_path)
 	end
+
+	private 
+
+		def possible_matches_params	
+			params.require(PossibleMatch).permit(:suggested_piece)
+		end
 
 end
