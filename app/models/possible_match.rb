2917f3dfb7c74b6d69f13ebe7cab05804ddb9c8d
KO527 <Kwajo.oseiakoto@gmail.com>
2017-05-23 10:41:14 -0400

    updated possible_match model

--- app/models/possible_match.rb
+++ app/models/possible_match.rb
@@ -4,8 +4,25 @@ class PossibleMatch < ActiveRecord::Base
 	validates :suggested_piece_id, presence: true
 	validates :contemplated_piece_id, presence: true
 
-	has_many :images
-	accepts_nested_parameters_for :image, reject_if: proc {|attributes| attributes[:type_of_image] != :show_picture !! :front_shot}
+	has_many :images, -> {where(type_of_image: 'show_picture')}
+	# accepts_nested_parameters_for :image, reject_if: proc {|attributes| attributes[:type_of_image] != :show_picture !! :front_shot}
+	private
+	
+		def next_suggested_male
+	  		Gor_Clothing.where("gor_clothings.id >= ? AND gor_clothing.gender = ?", gor_clothing.first.id, male).order("gor_clothings.id ASC")
+		end
+
+		def previous_suggested_male
+			Gor_Clothing.where("gor_clothings.id <= ? AND gor_clothing.gender = ?", gor_clothing.last.id, male).order("gor_clothings.id DESC")
+		end
+
+		def next_suggested_female
+			Gor_Clothing.where("gor_clothings.id >= ? AND gor_clothing.gender = ?", gor_clothing.first.id, female).order("gor_clothings.id ASC")
+		end
+
+		def previous_suggested_female
+		 	Gor_Clothing.where("gor_clothings.id <= ? AND gor_clothing.gender = ?", gor_clothing.last.id, female).order("gor_clothings.id DESC")
+		end
 
 end
 
