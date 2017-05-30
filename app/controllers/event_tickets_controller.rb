cdceaf16090080e3f93762ac0de9cc447bb5d556
KO527 <Kwajo.oseiakoto@gmail.com>
2017-05-29 22:04:17 -0400

    updated event_tickets controller

--- app/controllers/event_tickets_controller.rb
+++ app/controllers/event_tickets_controller.rb
@@ -2,6 +2,7 @@ class EventTicketsController < ApplicationController
 	require 'Time'
 	require '../models/event_tickets_api'
 	before_filter :format_time, :only => [:create]
+	respond_to :html, :js
 
 	def index #SuggEvents
 		Event_tickets.joins(:artists, :performers, :teams).where(status: 'favorited')
@@ -11,55 +12,12 @@ class EventTicketsController < ApplicationController
 		@ImmEvents = Event_tickets.GiveMeImmEvents
 		@PopularFests = Event_tickets.ParsePopularFests
 	end
-	
-	def create
-		#update preferences for remind_of_this_event or if favorited
-	end
 
-	def update
-		@Event_ticket = Event_ticket.find(params[:id])
-		if @Event_ticket.unfavorited?
-			@Event_ticket.status = :favorited
-			@Event_ticket.save
-			current_user.subscribe_to(@Event_ticket)
-			#if associated model is artist, performer or team, figure out how to increment or decrement fav_count or switch status
-		else
-			@Event_ticket.favorited!
-		end
-	end
-
-	private 
+	private
 
 		def format_time
 			happening = params[:happening]
 			params[:happening] = Time.strftime(happening, "%Y-%m-%d")
-
 		end
-
-
-# class EventTicketsApiController < ApplicationController
-  
-#   def index
-#   	@Event_ticketsImm = Event_tickets_api.GiveMeImmEvents
-#   	@Event_ticketsPopular = Event_tickets_api.ParsePopularFests
-#   end
-  
-#   def create
-#   	@search_item = params[:looking_for] || {}
-#   	@events = Event_tickets_api.for(@search_item)
-#   end
-
-#   def update
-  	
-#   end
-
-#   def destroy
-
-#   end
-
-#   private
-
-#   	params.require(:event_tickets_api).permit(:looking_for)
-# end
 
 end
