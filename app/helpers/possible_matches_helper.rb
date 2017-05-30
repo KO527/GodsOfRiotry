6d75eca2b3501c702aa0f66b545fce57016649c2
KO527 <Kwajo.oseiakoto@gmail.com>
2017-05-29 23:59:35 -0400

    removed unnecessary code

--- app/helpers/possible_matches_helper.rb
+++ app/helpers/possible_matches_helper.rb
@@ -1,12 +1,3 @@
 module PossibleMatchesHelper
 
-	def contemplated_piece
-		@possible_match = PossibleMatch.new(contemplated_piece_id: params[:gor_clothing_id])
-	end
-
-	def possible_matches
-		@gor_clothing = Gor_Clothing.find(params[:contemplated_piece_id])
-		@possible_matches = @gor_clothing.suggested_pieces(:all)
-	end
-
 end
