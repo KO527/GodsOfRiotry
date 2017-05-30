8e1ffb67440b38a87d377f67167619edd4bd9eb4
KO527 <Kwajo.oseiakoto@gmail.com>
2017-05-29 22:05:05 -0400

    updated playlist controller

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -1,5 +1,18 @@
 class PlaylistController < ApplicationController
 
 
+	def change_up
+		 @playlist = playlist.find_by(params[:id])
+		 @songs = @playlist.songs.all
+		 @songs.each do |song|
+	   	  	if song.kept_song!
+	   	 		songs_to_be_deleted << song
+	   	 	else
+	   	 	 	nextl
+	   	 	end
+	   	 end
+	               # songs_to_be_deleted.destroy  => collapse songs in playlist through javascript
+   	end
+
 	
 end
