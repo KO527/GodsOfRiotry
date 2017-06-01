5f1f270d11fef2d25545d3f1d4f16a40f615bdd6
KO527 <Kwajo.oseiakoto@gmail.com>
2017-06-01 19:51:02 -0400

    updated event_tickets controller

--- app/controllers/event_tickets_controller.rb
+++ app/controllers/event_tickets_controller.rb
@@ -1,6 +1,7 @@
 class EventTicketsController < ApplicationController
 	require 'Time'
 	require '../models/event_tickets_api'
+	
 	before_filter :format_time, :only => [:create]
 	respond_to :html, :js
 
@@ -11,6 +12,10 @@ class EventTicketsController < ApplicationController
 	def index
 		@ImmEvents = Event_tickets.GiveMeImmEvents
 		@PopularFests = Event_tickets.ParsePopularFests
+	end
+
+	def artist_events
+		@Event_tickets = Event_tickets.EventsByArtist.find(params[:performer_name])
 	end
 
 	private
