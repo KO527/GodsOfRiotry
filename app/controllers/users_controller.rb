ee98ca2acad513cf90086938842238828a7bd7a4
KO527 <Kwajo.oseiakoto@gmail.com>
2017-08-04 21:56:48 -0400

    updated users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,25 +39,58 @@ class UsersController < ApplicationController
 			render 'edit'
 		end
 	end
-#before filters
 
 	private
 
-	def logged_in_user
-	    unless logged_in?
-	    	store_location
-	    	flash[:danger] = "Please log in."
-	    	redirect_to login_url
-	    end
-	end
+		def next_piece_of_info
+			respond_to do |format|
+				unless logged_in?
+						render partial: 'basic_info'
+						format.js
+					if basic_info_params.valid?
+						render partial: 'gender'
+						format.js
+					elsif gender_params.valid?
+						render partial: 'artists_preferences'
+						format.js
+					elsif artists_preferences.valid?
+						render partial: 'soundcloud_connect'
+						format.js
+					else
+						return;
+					end
+				end
+			end
+		end
 
-	def correct_user
-		@user = User.find(params[:id])
-		redirect_to(root_url) unless current_user?(@user)
-	end
+		def gender_params
+			params.require(:user).permit(:gender)
+		end
+
+		def basic_info_params
+			params.require(:user).permit(:fullname, :email, :password, :password_confirmation)
+		end
+
+		def artists_preferences_params
+			params.require(:user).permit(:artist1, :artist2, :artist3, :artist4, :artist5)
+		end
 
 
-	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :uid, :provider_location, :provider_full_name, :provider_nickname, :access_token)
-	end
+		def logged_in_user
+		    unless logged_in?
+		    	store_location
+		    	flash[:danger] = "Please log in."
+		    	redirect_to login_url
+		    end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
+		end
+
+
+		def user_params
+			params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :uid, :provider_location, :provider_full_name, :provider_nickname, :access_token)
+		end
 end
