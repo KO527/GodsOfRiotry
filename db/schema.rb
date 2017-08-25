81d8bba6add044db1a42ff6304d12904acb04484
KO527 <Kwajo.oseiakoto@gmail.com>
2017-08-25 16:52:45 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,65 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820002437) do
+ActiveRecord::Schema.define(version: 20170825104757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "acts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "acts_event_ticket", id: false, force: :cascade do |t|
-    t.integer "event_ticket_id", null: false
-    t.integer "act_id",          null: false
-  end
-
-  add_index "acts_event_ticket", ["act_id", "event_ticket_id"], name: "index_acts_event_ticket_on_act_id_and_event_ticket_id", using: :btree
-  add_index "acts_event_ticket", ["event_ticket_id", "act_id"], name: "index_acts_event_ticket_on_event_ticket_id_and_act_id", using: :btree
 
   create_table "artists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "fav_count"
+    t.string   "name"
   end
-
-  create_table "artists_event_ticket", id: false, force: :cascade do |t|
-    t.integer "event_ticket_id", null: false
-    t.integer "artist_id",       null: false
-  end
-
-  add_index "artists_event_ticket", ["artist_id", "event_ticket_id"], name: "index_artists_event_ticket_on_artist_id_and_event_ticket_id", using: :btree
-  add_index "artists_event_ticket", ["event_ticket_id", "artist_id"], name: "index_artists_event_ticket_on_event_ticket_id_and_artist_id", using: :btree
-
-  create_table "event_ticket_teams", id: false, force: :cascade do |t|
-    t.integer "event_ticket_id", null: false
-    t.integer "team_id",         null: false
-  end
-
-  add_index "event_ticket_teams", ["event_ticket_id", "team_id"], name: "index_event_ticket_teams_on_event_ticket_id_and_team_id", using: :btree
-  add_index "event_ticket_teams", ["team_id", "event_ticket_id"], name: "index_event_ticket_teams_on_team_id_and_event_ticket_id", using: :btree
 
   create_table "event_tickets", force: :cascade do |t|
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "venue"
     t.datetime "happening"
     t.string   "name"
     t.string   "artist_id"
-    t.string   "artist_name"
-    t.string   "home_team"
-    t.string   "away_team"
     t.integer  "fav_count"
     t.integer  "user_id"
-    t.string   "performer_id"
   end
 
-  add_index "event_tickets", ["artist_name", "name", "happening"], name: "index_event_tickets_on_artist_name_and_name_and_happening", unique: true, using: :btree
-  add_index "event_tickets", ["away_team", "away_team", "happening"], name: "index_event_tickets_on_away_team_and_away_team_and_happening", unique: true, using: :btree
   add_index "event_tickets", ["happening"], name: "index_event_tickets_on_happening", unique: true, using: :btree
-  add_index "event_tickets", ["home_team", "away_team", "happening"], name: "index_event_tickets_on_home_team_and_away_team_and_happening", unique: true, using: :btree
   add_index "event_tickets", ["user_id"], name: "index_event_tickets_on_user_id", using: :btree
 
   create_table "gor_clothings", force: :cascade do |t|
@@ -127,12 +92,6 @@ ActiveRecord::Schema.define(version: 20170820002437) do
 
   add_index "preferences", ["user_id"], name: "index_preferences_on_user_id", using: :btree
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "songs", force: :cascade do |t|
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
@@ -161,6 +120,7 @@ ActiveRecord::Schema.define(version: 20170820002437) do
     t.boolean  "admin"
     t.string   "full_name"
     t.integer  "gender",          default: 0, null: false
+    t.integer  "role",            default: 0
   end
 
   create_table "wardrobes", force: :cascade do |t|
