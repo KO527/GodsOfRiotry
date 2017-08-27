bb6386def6415ada4c70b6ebdf15835d2ed909e3
KO527 <Kwajo.oseiakoto@gmail.com>
2017-08-27 02:00:03 -0400

    updated preference model

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,7 +1,7 @@
 class Preference < ActiveRecord::Base
 
 	belongs_to :subscriber_id, class_name: "User", counter_cache: true
-	has_many :favorited_artists, -> {where status: favorited}, class_name: "Artists", dependent: :destroy
+	has_many :favorited_artists, -> {where status: favorited}, class_name: "Artist", dependent: :destroy
 	has_many :favorited_songs, -> {where status: favorited}, class_name: "Song", dependent: :destroy
 	has_many :favorited_events, -> {where status: favorited}, class_name: "Event_ticket",  dependent: :destroy
 	has_many :favorited_outfits, -> {where status: favorited}, class_name: "Gor_Clothing", dependent: :destroy
@@ -9,8 +9,8 @@ class Preference < ActiveRecord::Base
 	validates :subscriber_id, presence: true
 	validates :fav_count, presence: true, numericality: {integer: true}, allow_nil: true
 	
-	accepts_nested_attributes_for :artists
-	accepts_nested_attributes_for :events
+	accepts_nested_attributes_for :favorited_artists
+	accepts_nested_attributes_for :favorited_events
 
 	#Scope is employed to help user find other users with similar tastes
 	#FIgure out how to come up with user's preferences that share three of the subscriber_id's favorited songs
