202d76ee094a9ebbf68eeb9bd9ee120822149c15
Kwajo Osei-Akoto <kwajo.oseiakoto@gmail.com>
2017-12-28 22:00:34 +0000

    moved controllers to another part of the project

--- app/controllers/soundcloud_controller.rb
+++
@@ -1,8 +0,0 @@
-class SoundcloudController < ApplicationController
-
-    def show
-      @client = SoundCloud.new(:client_id => ENV['SOUNDCLOUD_CLIENT_ID'], :client_secret => ENV['SOUNDCLOUD_CLIENT_SECRET'], :username => ENV['USERNAME'], :password => ENV['PASSWORD'])
-      render @playlist = @client.get('me/playlists').first
-    end       
-       
-end
